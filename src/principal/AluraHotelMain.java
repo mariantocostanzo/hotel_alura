eae60120f517455498a1aca87c426aafefe98a42
negucostanzo@hotmail.com <negucostanzo@hotamil.com>
2022-10-03 19:56:43 -0300

    hotel alura latam

---
+++ src/principal/AluraHotelMain.java
@@ -0,0 +1,25 @@
+package principal;
+
+import java.awt.EventQueue;
+import views.MenuPrincipal;
+
+public class AluraHotelMain {
+	
+
+	    public static void main(String[] args) {
+	        EventQueue.invokeLater(new Runnable() {
+	            public void run() {
+	                try {
+	                    MenuPrincipal frame = new MenuPrincipal();
+	                    frame.setVisible(true);
+	                } catch (Exception e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        });
+	    }
+
+	}
+
+
+
